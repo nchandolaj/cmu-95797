ad1eb0c0feddf7eed74887ace06b9c151b4d25c3
Nitin Chandola <nchandolaj@gmail.com>
2023-07-25 01:25:53 -0700

    modified sql to correctly calculate answers. NOTE: NULL values not handled.

--- nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
+++ nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
@@ -1,11 +1,19 @@
 select 
     weekday(pickup_datetime) as weekday,
     count(*) as total_trips,
-    sum( if((start_location.borough != end_location.borough), 1, 0)) as trips_with_different_burrough,
-    sum( if((start_location.borough != end_location.borough), 1, 0)) / count(*) as percentage_with_diff_start_end_borough
+    sum( 
+        case 
+            when start_location.borough != end_location.borough then 1 
+            else 0
+        end ) as trips_with_different_burrough,
+    sum( 
+        case 
+            when start_location.borough != end_location.borough then 1 
+            else 0
+        end ) * 100 / count(*) as percentage_with_diff_start_end_borough
 from {{ ref('mart__fact_all_taxi_trips') }} as taxi_trips
     left join {{ ref('mart__dim_locations') }} as start_location
         on taxi_trips.pulocationid = start_location.locationid
     left join {{ ref('mart__dim_locations') }} as end_location
-        on taxi_trips.pulocationid = end_location.locationid
+        on taxi_trips.dolocationid = end_location.locationid
 group by 1
