8ed4b58199f3a4273b4939ea3db561934ccad1bf
Nitin Chandola <nchandolaj@gmail.com>
2023-07-30 23:40:58 -0700

    Solution with only 50 zones. unfortunately the complete set of zones results on IO Error.

--- nyc_transit/analyses/average_time_between_pickups.sql
+++ nyc_transit/analyses/average_time_between_pickups.sql
@@ -8,12 +8,22 @@ then use this result to calculate the average time between pick ups per zone.
 */
 
 -- filter by zones and run it individually.
+{%- set all_zones = get_all_zones() %} 
 
-{%- set all_zones = get_all_zones() -%} 
+with renamed as (
+    {%- for current_zone in all_zones %}
+        select pl.zone as zone, 
+        t.pickup_datetime as current_pu_ts,
+        LAG(t.pickup_datetime, 1)
+           over(partition by pl.zone order by t.pickup_datetime) as next_pu_ts,
+        from {{ ref('mart__fact_all_taxi_trips') }} as t
+        join {{ ref('mart__dim_locations') }} as pl on pl.locationid = t.pulocationid
+        where pl.zone = '{{escape_single_quotes(current_zone)}}'
+        {% if not loop.last %} union all {% endif -%}
+    {% endfor %} 
+)
 
-{%- for current_zone in all_zones %}
-    select pl.zone 
-    from {{ ref('mart__fact_all_taxi_trips') }} t
-    join {{ ref('mart__dim_locations') }} pl on (pl.locationid = t.pulocationid)
-    where pl.zone = '{{current_zone}}'
-{% endfor %}
+select zone, 
+    avg(datediff('minute', next_pu_ts, current_pu_ts)) as average_time_between_pickups_for_zone
+from renamed
+group by 1
